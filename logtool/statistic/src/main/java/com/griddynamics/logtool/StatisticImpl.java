f744a53cde7c60fc90eda785a517e8d4014cbc08
avyshkin <avyshkin@griddynamics.com>
2012-02-14 18:41:09 +0400

    statistic implementation

--- logtool/statistic/src/main/java/com/griddynamics/logtool/StatisticImpl.java
+++ logtool/statistic/src/main/java/com/griddynamics/logtool/StatisticImpl.java
@@ -5,6 +5,8 @@ import org.apache.solr.schema.DateField;
 import java.util.*;
 
 import org.joda.time.*;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
 /**
  * Created by IntelliJ IDEA.
@@ -15,16 +17,27 @@ import org.joda.time.*;
  */
 public class StatisticImpl implements Statistic{
     private SearchServer searchServer;
+    private final DateTimeFormatter timeFormatter = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss'Z'");
     public StatisticImpl(SearchServer searchServer){
         this.searchServer = searchServer;
     }
-    private String makeRequest(Map<String,String> map){
-        String temp = "";
-        for(String fieldName: map.keySet()){
-        //here we shoud make request from map
-            temp = temp + map.get(fieldName);
+    private String makeRequest(Map<String,List<String>> map,long startTime,long endTime){
+        String query = "";
+        for (String key : map.keySet()) {
+          String oneTagQuery = "";
+          for (String value : map.get(key)) {
+            oneTagQuery += key + ":" + value + " OR ";
+          }
+          query = oneTagQuery.substring(0, oneTagQuery.length() - 4);
+          query += " AND ";
         }
-        return temp;
+        DateTime dt = new DateTime(startTime);
+        String startTimeString = timeFormatter.print(dt);
+        dt = new DateTime(endTime);
+        String endTimeString = timeFormatter.print(dt);
+        query = query + "timestamp:[" + startTimeString + " TO " + endTimeString +  "]";
+        //query = query.substring(0, query.length() - 5);
+        return query;
     }
     public StatisticResult makeStatistic(StatisticQuery query){
         List<Integer> result = new LinkedList<Integer>();
@@ -33,7 +46,7 @@ public class StatisticImpl implements Statistic{
         long timePart = (startTime.getMillis() - endTime.getMillis()) / query.getStepDiscretization();
 
         for (long i= startTime.getMillis();i<endTime.getMillis();i = i + timePart){
-            List<Map<String,String>> list = searchServer.search(makeRequest(query.getQuery()),-1,-1,"","");
+            List<Map<String,String>> list = searchServer.search(makeRequest(query.getQuery(),i,i + timePart),-1,-1,"","");
             result.add(new Integer(list.size()));
         }
         return new StatisticResult(result);
--- logtool/statistic/src/main/java/com/griddynamics/logtool/StatisticQuery.java
+++ logtool/statistic/src/main/java/com/griddynamics/logtool/StatisticQuery.java
@@ -2,6 +2,7 @@ package com.griddynamics.logtool;
 
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -12,7 +13,7 @@ import java.util.Map;
  * To change this template use File | Settings | File Templates.
  */
 public class StatisticQuery {
-    private Map<String,String> query;
+    private Map<String,List<String>> query;
     private Integer stepDiscretization;
     private String startDate;
     private String endDate;
@@ -20,13 +21,13 @@ public class StatisticQuery {
     *   startDate and endDate is date in solr date format
     *   stepDiscretization - is a number of parts into which time period divided
     */
-    public StatisticQuery(Map<String,String> query,Integer stepDiscretization,String startDate,String endDate){
+    public StatisticQuery(Map<String,List<String>> query,Integer stepDiscretization,String startDate,String endDate){
         this.query = query;
         this.stepDiscretization = stepDiscretization;
         this.startDate = startDate;
         this.endDate = endDate;
     }
-    public Map<String,String> getQuery(){
+    public Map<String,List<String>> getQuery(){
         return query;
     }
     public Integer getStepDiscretization(){
